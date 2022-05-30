42ef8cf2e31c4a459767d76e01e913e1b9b832f9
Robi <robi.maliqi@hotmail.co.uk>
2022-05-30 18:02:28 +0100

    Walkthrough – Sinatra: Introducing params - changing the parameters

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/cat' do
-  @name = ["Amigo", "Misty", "Almond"]sample
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
