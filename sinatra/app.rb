e16c346a0eaaf1e8c659ae09b023e7c27941d2ef
Robi <robi.maliqi@hotmail.co.uk>
2022-05-30 17:07:28 +0100

    Walkthrough – Sinatra: Views - Created index.erb file

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -2,7 +2,5 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/cat' do
-  "<div style='border: 4mm ridge rgba(211, 220, 50, .6);'>
-     <img src='https://i.imgur.com/jFaSxym.png'>
-   </div>"
+  erb(:index)
 end
