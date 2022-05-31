76db4a5c5eb8afd2e966c0523a4291c7fc119299
Robi <robi.maliqi@hotmail.co.uk>
2022-05-31 13:06:07 +0100

    Sinatra: POSTed params - creating a POST method that links with a GETS method

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -6,9 +6,16 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
 
+get '/secret' do
+  "hello world"
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
